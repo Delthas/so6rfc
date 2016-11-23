426857e2fbdb65770b48b980e7d7bb2dcd9606c2
delthas <delthas@saucisseroyale.fr.cr>
2016-11-23 04:05:00 +0100

    adds more debug to travis.yml

--- scripts/upload-so6rfc.sh
+++ scripts/upload-so6rfc.sh
@@ -3,11 +3,11 @@ set -ev
 echo "$FTP_PASSWORD"
 echo "$FTP_USER"
 echo "cc" > output.txt
-sshpass -p "$FTP_PASSWORD" scp -q output.txt "$FTP_USER"@delthas.fr:/srv/http/so6rfc/ccsava.txt
+sshpass -p "$FTP_PASSWORD" scp -o StrictHostKeyChecking=no -q output.txt "$FTP_USER"@delthas.fr:/srv/http/so6rfc/ccsava.txt
 in=ppr.xml
-sshpass -p "$FTP_PASSWORD" scp -q output.txt "$FTP_USER"@delthas.fr:/srv/http/so6rfc/$(echo $in | grep -o '^[^.]*' | tr '[a-z]' '[A-Z]').txt
+sshpass -p "$FTP_PASSWORD" scp -o StrictHostKeyChecking=no -q output.txt "$FTP_USER"@delthas.fr:/srv/http/so6rfc/$(echo $in | grep -o '^[^.]*' | tr '[a-z]' '[A-Z]').txt
 git diff --name-only $TRAVIS_COMMIT_RANGE | grep 'src/main/resources/so6rfc[0-9]*\.xml' | grep -o '[^/]*$' | while read in
 do
     java -jar target/so6rfc-1.0.0-jar-with-dependencies.jar $in > output.txt
-    sshpass -p "$FTP_PASSWORD" scp -q output.txt "$FTP_USER"@delthas.fr:/srv/http/so6rfc/$(echo $in | grep -o '^[^.]*' | tr '[a-z]' '[A-Z]').txt
+    sshpass -p "$FTP_PASSWORD" scp -o StrictHostKeyChecking=no -q output.txt "$FTP_USER"@delthas.fr:/srv/http/so6rfc/$(echo $in | grep -o '^[^.]*' | tr '[a-z]' '[A-Z]').txt
 done
