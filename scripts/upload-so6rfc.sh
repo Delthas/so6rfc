e7b49a9e83e35b38daa0b7b0b3d480938a6be2d0
delthas <delthas@saucisseroyale.fr.cr>
2016-11-23 03:33:49 +0100

    fixes travis.yml

--- scripts/upload-so6rfc.sh
+++ scripts/upload-so6rfc.sh
@@ -3,5 +3,5 @@ set -ev
 git diff --name-only $TRAVIS_COMMIT_RANGE | grep 'src/main/resources/so6rfc[0-9]*\.xml' | grep -o '[^/]*$' | while read in
 do
     java -jar target/so6rfc-1.0.0-jar-with-dependencies.jar $in > output.txt
-    curl -T output.txt -u $FTP_USER:$FTP_PASSWORD sftp://delthas.fr/srv/http/so6rfc/$(echo $in | grep -o '^[^.]*' | tr '[a-z]' '[A-Z]').txt
+    sshpass -p $FTP_PASSWORD scp output.txt $FTP_USER@delthas.fr:/srv/http/so6rfc/$(echo $in | grep -o '^[^.]*' | tr '[a-z]' '[A-Z]').txt
 done
