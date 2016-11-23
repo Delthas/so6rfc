4da3005d73d43b3fe4c9d7e76af85313c4d0cd75
delthas <delthas@saucisseroyale.fr.cr>
2016-11-23 03:03:09 +0100

    adds automatic deploy to so6rfc server with travis.yml

---
+++ scripts/upload-so6rfc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ev
+git diff --name-only $TRAVIS_COMMIT_RANGE | grep 'src/main/resources/so6rfc[0-9]*\.xml' | grep -o '[^/]*$' | while read in
+do
+    java -jar target/so6rfc-1.0.0-jar-with-dependencies.jar in > output.txt
+    curl -T output.txt -u $FTP_USER:$FTP_PASSWORD sftp://delthas.fr/srv/http/so6rfc/$(echo in | grep -o '^[^.]*' | tr '[a-z]' '[A-Z]').txt
+done
